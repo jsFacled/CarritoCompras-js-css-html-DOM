1249d409809e0c8a20be4538abdf09d060a4988c
jsFacled <facled@gmail.com>
2022-07-24 20:11:49 -0300

    creando carrito al clickear Comprar

--- README.txt
+++ README.txt
@@ -1,6 +1,8 @@
 https://www.youtube.com/watch?v=JL7Wo-ASah4&list=PLPl81lqbj-4I2ZOzryjPKxfhK3BzTlaJ7&index=9
 
-22:27
+36
+
+
 
 
 
--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const items=document.getElementById("items")
 const templateCard=document.getElementById("template-card").content
 const fragment = document.createDocumentFragment()
+let carrito ={}
+
 
 
 document.addEventListener("DOMContentLoaded",()=>{
     fetchData()
+})
+
+items.addEventListener("click",e=>{
+    addCarrito(e)
 })
 
 const fetchData=async ()=>{
@@ -21,10 +27,36 @@ const fetchData=async ()=>{
 const pintarCards = data =>{
     data.forEach(producto => {
         templateCard.querySelector("h5").textContent=producto.title;
+        templateCard.querySelector("p").textContent=producto.precio;
+        templateCard.querySelector("img").setAttribute("src",producto.thumbnailUrl);
+        templateCard.querySelector(".btn-dark").dataset.id=producto.id;
+
 
         const clone=templateCard.cloneNode(true)
         fragment.appendChild(clone)
     });
     items.appendChild(fragment)
+}
+
+const addCarrito = e =>{
+    
+    if(e.target.classList.contains("btn-dark")){
+        setCarrito(e.target.parentElement)
+    }
+
+    e.stopPropagation()
+}
+
+const setCarrito = objeto =>{
+    console.log(objeto);
+    const producto ={
+       id: objeto.querySelector(".btn-dark").dataset.id,
+       title: objeto.querySelector("h5").textContent,
+       precio: objeto.querySelector("p").textContent,
+       cantidad: 1
+
+    }
+    console.log(producto);
+
 
 }
